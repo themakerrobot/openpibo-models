4573978d6ae6cda59266354ae12665e0f2425713
Yunjai, Lee <leeyunjai@circul.us>
2024-12-10 16:13:09 +0900

    Update setup.py

--- openpibo_models/setup.py
+++ openpibo_models/setup.py
@@ -34,6 +34,10 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',        
     ],
     test_suite='tests',
     tests_require=test_requirements
